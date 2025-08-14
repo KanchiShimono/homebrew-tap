ce181eaca641a7c22726146c35f49e8b1f293cdf
Kanchi Shimono <17161397+KanchiShimono@users.noreply.github.com>
2025-08-14 14:45:26 +0900

    mousequake v0.0.4

--- Formula/mousequake.rb
+++ Formula/mousequake.rb
@@ -1,23 +1,28 @@
 class Mousequake < Formula
-  version "0.0.3"
-  desc "Simple tool for auto shaking mouse pointer"
+  version "0.0.4"
+  desc "Simple tool for automatically shaking the mouse pointer"
   homepage "https://github.com/KanchiShimono/mousequake"
   license ""
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/KanchiShimono/mousequake/releases/download/v#{version}/mousequake-x86_64-apple-darwin.tar.gz"
-      sha256 "fc9f7db523958ddb385c999baba40a145f7e14af349eb3b7173d91e0cab5c6e1"
+      sha256 "262527fab2eec4403d23fc922760cb02c5bbffbee9e095c554fdc8c2fce9db25"
     elsif Hardware::CPU.arm?
       url "https://github.com/KanchiShimono/mousequake/releases/download/v#{version}/mousequake-aarch64-apple-darwin.tar.gz"
-      sha256 "cebd4a781a5d16790171983133a6cc09adec5bd34a563e4bbc048142b6d961b5"
+      sha256 "d346586e807d2093848c1357f10b4eef770426a82d7b7a00029527e9946204c9"
     end
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/KanchiShimono/mousequake/releases/download/v#{version}/mousequake-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "cf6760ef9e1b5ccadbd11a44473d343071fb450f4055dfd73b0347824bba9f11"
+    sha256 "2c8d802538b0aa45f81fb28d219e51dc00f87bad301f93ad6a82be2e61f66e0d"
   end
 
   def install
     bin.install "mousequake"
+    generate_completions_from_executable(bin/"mousequake", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mousequake --version", 2)
   end
 end
