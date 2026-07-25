998d0384b46a949cc2f5d2a6cf72ddf0689b0cec
Kanchi Shimono <17161397+KanchiShimono@users.noreply.github.com>
2026-07-26 02:50:11 +0900

    Fixes architecture detection in assam and mousequake

--- Formula/assam.rb
+++ Formula/assam.rb
@@ -1,25 +1,24 @@
 class Assam < Formula
   desc "AWS SAML authentication CLI tool for Azure Entra ID"
-  version "0.0.1"
   homepage "https://github.com/KanchiShimono/assam-rs"
+  version "0.0.1"
   license ""
 
   on_macos do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/KanchiShimono/assam-rs/releases/download/v#{version}/assam-x86_64-apple-darwin.tar.gz"
       sha256 "01f8240e5d4c9f8a990cee107ebe397dbf8e185d45b91fce82afdbf916c2e88d"
-    elsif Hardware::CPU.arm?
+    end
+    on_arm do
       url "https://github.com/KanchiShimono/assam-rs/releases/download/v#{version}/assam-aarch64-apple-darwin.tar.gz"
       sha256 "d2fd2b395c5e48a8370e4a1c584177c925b56a3d57b445ee2255492afbae0635"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/KanchiShimono/assam-rs/releases/download/v#{version}/assam-x86_64-unknown-linux-musl.tar.gz"
       sha256 "1c54d96e2dfa2643971e435c50f736309915a706d201f833f429645c9f054e39"
-    else
-      odie "assam is only available for x86_64 architecture on Linux"
     end
   end
 
@@ -29,6 +28,6 @@ class Assam < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/assam --version", 2)
+    assert_match version.to_s, shell_output("#{bin}/assam --version")
   end
 end
