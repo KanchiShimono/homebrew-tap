64e349f0313b2c869c207ba8d7da21171f2f310c
kanchi <17161397+KanchiShimono@users.noreply.github.com>
2026-07-26 03:08:51 +0900

    Fixes architecture detection in assam and mousequake (#10)

--- Formula/mousequake.rb
+++ Formula/mousequake.rb
@@ -1,25 +1,24 @@
 class Mousequake < Formula
-  version "0.0.5"
   desc "Simple tool for automatically shaking the mouse pointer"
   homepage "https://github.com/KanchiShimono/mousequake"
+  version "0.0.5"
   license ""
 
   on_macos do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/KanchiShimono/mousequake/releases/download/v#{version}/mousequake-x86_64-apple-darwin.tar.gz"
       sha256 "ddba6e2d6647c8d03eb53aedec27080ade241d71069158b30fbbd7933a6d0627"
-    elsif Hardware::CPU.arm?
+    end
+    on_arm do
       url "https://github.com/KanchiShimono/mousequake/releases/download/v#{version}/mousequake-aarch64-apple-darwin.tar.gz"
       sha256 "11d023712b9210d00b7ccb2f250ea7c02ac7f228531a51e21413ec8500d3a729"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
+    on_intel do
       url "https://github.com/KanchiShimono/mousequake/releases/download/v#{version}/mousequake-x86_64-unknown-linux-musl.tar.gz"
       sha256 "e91a9ce98906b4afb3885a02107128cd68f72137b7d9ea378a2007a5ffff5a8e"
-    else
-      odie "mousequake is only available for x86_64 architecture on Linux"
     end
   end
 
@@ -29,6 +28,6 @@ class Mousequake < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/mousequake --version", 2)
+    assert_match version.to_s, shell_output("#{bin}/mousequake --version")
   end
 end
